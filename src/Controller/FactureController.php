2dbd30d51c2b56ae0e8d4bc0ff009994b48d1501
Majidli Nijat <nijatmajidli@gmail.com>
2022-09-12 21:59:54 +0200

    new modif

--- src/Controller/FactureController.php
+++ src/Controller/FactureController.php
@@ -5,9 +5,7 @@ namespace App\Controller;
 use App\Repository\CommandeRepository;
 use App\Repository\LignedecommandeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 use Dompdf\Dompdf;
 use Dompdf\Options;
 
@@ -24,9 +22,9 @@ class FactureController extends AbstractController
         $pdfOptions->set([
             'defaultFont'=>'Arial', 
             'isRemoteEnabled'=>true, 
-            'isPhpEnabled'=>true,
-            'chroot'=>"images/facture/VillageGreen.jpg", 
-            ]);
+            'isPhpEnabled'=>true, 
+            'chroot'=>"images/facture/VillageGreen.jpg"
+        ]);
         
         // Instantiate Dompdf with our options
         $dompdf = new Dompdf($pdfOptions);
@@ -40,39 +38,13 @@ class FactureController extends AbstractController
         // Load HTML to Dompdf
         $dompdf->loadHtml($html);
         
-        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+        // (Optional) Setup the paper size and orientation 'portrait' or 'landscape'
         $dompdf->setPaper('A4', 'portrait');
 
         // Render the HTML as PDF
         $dompdf->render();
 
         // Output the generated PDF to Browser (force download)
-        $dompdf->stream("facture.pdf", [
-            "Attachment" => true,
-            "Content-type"
-        ]);
+        $dompdf->stream("facture.pdf", ["Attachment" => true,]);
     }
-
-
-
-    
-    
-    // create a PDF from HTML using SnappyBundle (wkhtmltopdf)
-    // use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
-    // use Knp\Snappy\Pdf;
-    /**
-     * @Route("/facture/{id}", name="facture")
-     */
-    // public function index($id, Pdf $knpSnappyPdf, CommandeRepository $commandeRepo, LignedecommandeRepository $lignedecommandeRepo)
-    // {
-    //     $html = $this->renderView('facture/index.html.twig', array(
-    //         'Commande' => $commandeRepo->find($id),
-    //         'Produit' => $lignedecommandeRepo->findBy(['com'=>$id])
-    //     ));
-
-    //     return new PdfResponse(
-    //         $knpSnappyPdf->getOutputFromHtml($html),
-    //         'file.pdf'
-    //     );
-    // }
 }

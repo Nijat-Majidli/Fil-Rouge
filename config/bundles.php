da3788f486bf09230b3e90e06d109161f2eb5bc2
Majidli Nijat <nijatmajidli@gmail.com>
2022-09-27 09:35:25 +0200

    new modif reset_password

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -55,7 +55,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('nijatmajidli@gmail.com', 'Village Green Mail'))
+                    ->from(new Address('no-reply@villagegreen.com', 'Village Green Mail'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
@@ -77,6 +77,7 @@ class RegistrationController extends AbstractController
             'categories' => $catRepo->findAll(),
         ]);
     }
+
 
     /**
      * @Route("/verify/email", name="app_verify_email")
